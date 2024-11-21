57b5111d494e10668e555aa324b28467fa793d7b
Raphael567 <raphaelcanuto464@gmail.com>
2024-11-20 23:34:30 -0300

    feat: GETById, PUT and DELETE

--- JsonPlaceHolder/JsonPlaceHolderAPP/Services/PostsService.cs
+++ JsonPlaceHolder/JsonPlaceHolderAPP/Services/PostsService.cs
@@ -1,6 +1,7 @@
 ﻿using JsonPlaceHolderAPP.Models;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Net.Http.Json;
 using System.Text.Json;
 
 namespace JsonPlaceHolderAPP.Services
@@ -26,20 +27,111 @@ namespace JsonPlaceHolderAPP.Services
                     string json = await response.Content.ReadAsStringAsync();
                     Debug.WriteLine("Posts carregados: " + json);
 
-                    var postsList = JsonSerializer.Deserialize<ObservableCollection<Post>>(json);
-                    if (postsList != null) 
-                    { 
+                    var postsList = JsonSerializer.Deserialize<List<Post>>(json);
+
+                    if (postsList != null)
+                    {
                         return new ObservableCollection<Post>(postsList);
                     }
+
+                    throw new Exception($"Erro ao carregar posts: {response.StatusCode}");
                 }
-            } 
+
+                throw new Exception($"Erro ao carregar posts. Status Code: {response.StatusCode}");
+            }
             catch (Exception ex)
             {
-                Debug.WriteLine($"Error: {ex.Message}");
-                Debug.WriteLine(ex.ToString());
+                throw new Exception("Erro ao fazer a requisição GET", ex);
             }
+        }
 
-            return null;
+        public async Task<Post> GetPostById(int id)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync($"posts/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+
+                    var post = JsonSerializer.Deserialize<Post>(json);
+
+                    if (post != null)
+                    {
+                        return post;
+                    }
+
+                    throw new Exception($"O post retornado está vazio");
+                }
+
+                throw new Exception($"Erro ao carregar post: {response.StatusCode}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao fazer a requisição GET por ID", ex);
+            }
+        }
+
+        public async Task<Post> CreatePost(Post newPost)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.PostAsJsonAsync("posts", newPost);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var createdPost = await response.Content.ReadFromJsonAsync<Post>();
+                    return createdPost;
+                }
+
+                throw new Exception($"Erro ao criar post: {response.StatusCode}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao fazer a requisição POST", ex);
+            }
+        }
+
+        public async Task<bool> UpdatePost(Post post)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.PutAsJsonAsync($"posts/{post.id}", post);
+
+                Debug.WriteLine($"Status Code: {response.StatusCode}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                else
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Debug.WriteLine($"Erro: {responseBody}");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Erro ao atualizar post: {ex.Message}");
+                return false;
+            }
+        }
+
+        public async Task<bool> DeletePost(int id)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.DeleteAsync($"posts/{id}");
+
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Erro ao deletar post: {ex.Message}");
+                return false;
+            }
         }
     }
 }
--- JsonPlaceHolder/JsonPlaceHolderAPP/ViewModels/PostsViewModel.cs
+++ JsonPlaceHolder/JsonPlaceHolderAPP/ViewModels/PostsViewModel.cs
@@ -3,7 +3,6 @@ using CommunityToolkit.Mvvm.Input;
 using JsonPlaceHolderAPP.Models;
 using JsonPlaceHolderAPP.Services;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Windows.Input;
 
 namespace JsonPlaceHolderAPP.ViewModels
@@ -16,16 +15,153 @@ namespace JsonPlaceHolderAPP.ViewModels
         {
             _postsService = new PostService();
             GetAllPostsCommand = new AsyncRelayCommand(LoadPosts);
+            UpdatePostAsyncCommand = new AsyncRelayCommand(UpdatePostAsync);
+            DeletePostAsyncCommand = new AsyncRelayCommand<int>(DeletePostAsync);
         }
 
         [ObservableProperty]
         private ObservableCollection<Post> _posts;
 
+        [ObservableProperty]
+        private int postId;
+
+        [ObservableProperty]
+        private int deletePostId;
+
+        [ObservableProperty]
+        private int updatePostId;
+
+        [ObservableProperty]
+        private string title;
+
+        [ObservableProperty]
+        private string body;
+
+        [ObservableProperty]
+        private string userId;
+
+        [ObservableProperty]
+        private string createPostTitle;
+
+        [ObservableProperty]
+        private string createPostBody;
+
         public ICommand GetAllPostsCommand { get; }
+        public ICommand UpdatePostAsyncCommand { get; }
+        public ICommand DeletePostAsyncCommand { get; }
 
         private async Task LoadPosts()
         {
             Posts = await _postsService.GetAllPosts();
+        }
+
+        [RelayCommand]
+        public async Task LoadPostById(int id)
+        {
+            try
+            {
+                var post = await _postsService.GetPostById(id);
+
+                if (post != null)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Post", $"Post: {post.title}\n\n{post.body}", "OK");
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Post", "Post não encontrado", "OK");
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Erro ao carregar post", ex);
+            }
+        }
+
+        [RelayCommand]
+        public async Task CreatePost()
+        {
+            var newPost = new Post
+            {
+                userId = Convert.ToInt32(userId),
+                title = createPostTitle,
+                body = createPostBody
+            };
+
+            try
+            {
+                var createdPost = await _postsService.CreatePost(newPost);
+
+                await Application.Current.MainPage.DisplayAlert("Post criado", $"Post criado com sucesso! Id: {createdPost.id}", "OK");
+
+                userId = string.Empty;
+                title = string.Empty;
+                body = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao criar post", ex);
+            }
+        }
+
+        public async Task UpdatePostAsync()
+        {
+            var postToUpdate = new Post
+            {
+                id = UpdatePostId,
+                title = Title,
+                body = Body
+            };
+
+            try
+            {
+                var success = await _postsService.UpdatePost(postToUpdate);
+
+                if (success)
+                {
+                    var updatedPost = Posts.FirstOrDefault(p => p.id == UpdatePostId);
+                    if (updatedPost != null)
+                    {
+                        updatedPost.title = Title;
+                        updatedPost.body = Body;
+
+                        await Application.Current.MainPage.DisplayAlert("Post atualizado com sucesso.",
+                            $"Post: {updatedPost.title}\n\n{updatedPost.body}", "OK");
+                    }
+                    else
+                    {
+                        await Application.Current.MainPage.DisplayAlert("Erro", "Post não encontrado após atualização.", "OK");
+                    }
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Erro", "Não foi possível atualizar o post.", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Erro", $"Erro ao atualizar post: {ex.Message}", "OK");
+            }
+        }
+
+
+        private async Task DeletePostAsync(int id)
+        {
+            bool success = await _postsService.DeletePost(id);
+
+            if (success)
+            {
+                var postToRemove = Posts.FirstOrDefault(p => p.id == id);
+                if (postToRemove != null)
+                {
+                    Posts.Remove(postToRemove);
+                }
+
+                await Application.Current.MainPage.DisplayAlert("Sucesso", "Post deletado com sucesso.", "OK");
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Erro", "Não foi possível deletar o post.", "OK");
+            }
         }
     }
 }
